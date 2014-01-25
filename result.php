8261d2580db9ee4111dab2dbe61d025f0213e249
yill84 <yiliang.he@gmail.com>
2014-01-25 17:17:17 +0800

    1.4

--- result.php
+++ result.php
@@ -9,13 +9,39 @@
 
 	$queueArray = strtok($queue, "、");
 	echo "没有去重的总人数是 ".count($queueArray)."人<br/>";
+
+	echo "没有去重search：".array_search($target, $queueArray);
 	
-	$aaaa = array("abc"=>1, "sss"=>2);
-	echo "aaaa数组的长度是".count($aaaa);
-	$bbbb = array("yyy"=>3);
-	$aaaa = array_merge($aaaa, $bbbb);
-	echo "aaaa数组的长度是".count($aaaa);
+	$queueArrayNoDup = array_unique($queueArray);
+	echo "去重的总人数是 ".count($queueArrayNoDup)."人<br/>";
+
+	echo "去重search：".array_search($target, $queueArrayNoDup);
 	
+	$i=1;
+	foreach ($queueArrayNoDup as $name)
+	{
+		if(0 == strcasecmp($target, $name))
+		{
+			echo $target."排在第".$i."位";
+		}
+		$i +=1;
+	}
+
+
+	/*
+	 foreach ($queueArray as $name)
+	 {
+	if(array_key_exist($name,$queueArrayNoDup))
+	{
+	$queueArrayNoDup[$name]+1;
+	}
+	else
+	{
+	$queueArrayNoDup = array_merge($queueArrayNoDup,array($name=>1));
+	}
+	$queueArrayNoDup;
+	} */
+
 	echo "queueArray is ".$queueArray;
 	echo "<br/>";
 	echo "target is ".$target;
