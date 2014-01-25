23b44c1759542089eb1647103a40f3f294f13564
yill84 <yiliang.he@gmail.com>
2014-01-25 17:36:30 +0800

    textarea

--- index.php
+++ index.php
@@ -2,15 +2,44 @@
 <head>
 </head>
 <body>
-ver:1.0<br/>
+	ver:1.0
+	<br />
+	<?php 
+	$queue = $_POST["queue"];
+	$target = $_POST["target"];
+	?>
 	<div>
-	<form action="result.php" method="post">
-		<p/>粘贴所有排队人员：<input name="queue" type="textarea"/> <br/>
-		<p/>要查询序号的名称：<input
-			name="target" type="text" /> <br/>
-		<p/><input type="submit" value="查询"/>
-	</form>
+		<form action="index.php" method="post">
+			<p />
+			粘贴所有排队人员：<textarea rows="3" cols="50" name="queue"><?php $queue?></textarea>
+			<br />
+			<p />
+			要查询序号的名称：<input name="target" type="text" value="<?php $target?>" />
+			<br />
+			<p />
+			<input type="submit" value="查询" />
+		</form>
 	</div>
-	
+
+	<div>
+		<?php 
+		$queueArray = explode("、", $queue);
+		echo "没有去重的总人数是".count($queueArray)."人<br/>";
+
+		$queueArrayNoDup = array_unique($queueArray);
+		echo "去重的总人数是".count($queueArrayNoDup)."人<br/>";
+
+		$i=1;
+		foreach ($queueArrayNoDup as $name)
+		{
+			if(0 == strcasecmp($target, $name))
+			{
+				echo "\"".$target."\"排在第".$i."位<br/>";
+			}
+			$i +=1;
+		}
+		?>
+
+	</div>
 </body>
 </html>
