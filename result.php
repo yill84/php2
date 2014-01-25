e7d5e0ac2a89dbcfcd6f0ba4e1d457af8aca3aff
yill84 <yiliang.he@gmail.com>
2014-01-25 16:51:28 +0800

    1.3

--- result.php
+++ result.php
@@ -8,7 +8,13 @@
 	$target = $_GET["target"];
 
 	$queueArray = strtok($queue, "、");
+	echo "没有去重的总人数是 ".count($queueArray)."人<br/>";
 	
+	$aaaa = array("abc"=>1, "sss"=>2);
+	echo "aaaa数组的长度是".count($aaaa);
+	$bbbb = array("yyy"=>3);
+	$aaaa = array_merge($aaaa, $bbbb);
+	echo "aaaa数组的长度是".count($aaaa);
 	
 	echo "queueArray is ".$queueArray;
 	echo "<br/>";
