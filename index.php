fc1e18160bd98c67619f1dd9d3e36bc9fdcc12d2
yill84 <yiliang.he@gmail.com>
2014-01-25 17:40:52 +0800

    空判断

--- index.php
+++ index.php
@@ -11,7 +11,10 @@
 	<div>
 		<form action="index.php" method="post">
 			<p />
-			粘贴所有排队人员：<textarea rows="3" cols="50" name="queue"><?php $queue?></textarea>
+			粘贴所有排队人员：
+			<textarea rows="3" cols="50" name="queue">
+				<?php $queue?>
+			</textarea>
 			<br />
 			<p />
 			要查询序号的名称：<input name="target" type="text" value="<?php $target?>" />
@@ -23,20 +26,24 @@
 
 	<div>
 		<?php 
-		$queueArray = explode("、", $queue);
-		echo "没有去重的总人数是".count($queueArray)."人<br/>";
 
-		$queueArrayNoDup = array_unique($queueArray);
-		echo "去重的总人数是".count($queueArrayNoDup)."人<br/>";
-
-		$i=1;
-		foreach ($queueArrayNoDup as $name)
+		if(0 != strcasecmp("",$queue) && 0 != strcasecmp("",$target))
 		{
-			if(0 == strcasecmp($target, $name))
+			$queueArray = explode("、", $queue);
+			echo "没有去重的总人数是".count($queueArray)."人<br/>";
+
+			$queueArrayNoDup = array_unique($queueArray);
+			echo "去重的总人数是".count($queueArrayNoDup)."人<br/>";
+
+			$i=1;
+			foreach ($queueArrayNoDup as $name)
 			{
-				echo "\"".$target."\"排在第".$i."位<br/>";
+				if(0 == strcasecmp($target, $name))
+				{
+					echo "\"".$target."\"排在第".$i."位<br/>";
+				}
+				$i +=1;
 			}
-			$i +=1;
 		}
 		?>
 
