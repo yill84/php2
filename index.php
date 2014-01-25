c192593592d11da8b43ed786c77cd6a157dfd1d9
yill84 <yiliang.he@gmail.com>
2014-01-25 16:16:05 +0800

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+</head>
+<body>
+	<div>
+	<form action="result.php" method="get">
+		<p/>粘贴所有排队人员：<input name="queue" type="textarea"/> <br/>
+		<p/>要查询序号的名称：<input
+			name="target" type="text" /> <br/>
+		<p/><input type="submit" value="查询"/>
+	</form>
+	</div>
+
+
+	
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,19 @@
+<html>
+<head></head>
+
+<body>
+
+<?php 
+$queue=$_GET["queue"];
+$target=$_GET["target"];
+
+echo "queue is ".$queue;
+echo "<br/>";
+echo "target is ".$target;
+
+
+?>
+
+</body>
+
+</html>
