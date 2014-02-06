51d1c7a7d1b195d080dd36f526c53b93e2dab815
yill84 <yiliang.he@gmail.com>
2014-02-06 14:00:28 +0800

    s

--- shouyi.php
+++ shouyi.php
@@ -1,1 +1,28 @@
-hello
+<?php 
+//年化收益率=一年收益/本金×100%
+//一年天数=365
+//收益=(年华收益率×本金×天数)/一年天数
+//年华收益率=(收益×一年天数)/(本金×天数)
+
+$shouyi=$_POST("shouyi");
+$benjin=$_POST("benjin");;
+$day=$_POST("day");;
+
+$year=365;
+$nianhua;
+
+?>
+
+<div>
+	<form action="shouyi.php" method="POST">
+		本金：<input type="text" value="<?php echo $benjin;?>"/><br/>
+		天数：<input type="text" value="<?php echo $day;?>"/><br/>
+		收益：<input type="text" value="<?php echo $shouyi;?>"/><br/>
+		
+		<input type="submit" value="计算"/>		
+	</form>
+</div>
+
+<div>
+		年华收益率：<?php echo $nianhua;?>
+</div>
