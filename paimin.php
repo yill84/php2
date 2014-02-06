1566ca78fbb9f191c8c3528dce54b3ead733ec1c
yill84 <yiliang.he@gmail.com>
2014-02-06 16:23:28 +0800

    1

--- paimin.php
+++ paimin.php
@@ -111,6 +111,7 @@
 	if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete")
 	 { 
 		alert("ori="+xmlHttp.responseText);
+		alert("oriXml="+xmlHttp.responseXml);
 		xmlHttp1=GetXmlHttpObject();
 		if (xmlHttp1==null)
 		  {
