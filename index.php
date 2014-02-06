fb5b3d6c26078a9d0d5b1ba1338f8be7e7514fdf
yill84 <yiliang.he@gmail.com>
2014-02-06 14:19:45 +0800

    utf-8

--- index.php
+++ index.php
@@ -3,11 +3,34 @@
 </head>
 <body>
 	ver:1.0
+	
 	<br />
 	<?php 
+	//http://www.elimautism.org/news_content4.asp?id=119
+	/*
+	 * 
+<P style="TEXT-ALIGN: left; LINE-HEIGHT: 160%; MARGIN: 0cm 0cm 0pt; mso-pagination: widow-orphan" class=MsoNormal align=left></SPAN><SPAN style="LINE-HEIGHT: 160%; FONT-FAMILY: 宋体; FONT-SIZE: 9pt; mso-font-kerning: 0pt; mso-bidi-font-family: 宋体"><FONT face="Times New Roman">田浩琳、
+	 */
 	$start = gettimeofday();
-	$queue = trim($_POST["queue"]);
+	
+	$queue = $response;//trim($_POST["queue"]);
+	$queueArray = explode("、", $queue);
+	
+	//取array第一个
+	$firstName = $queueArray[0];
+	$lastSplit = strrchr($firstName,">");
+	$firstName = substr($lastSplit,1);
+	$queueArray[0] = $firstName;
+	
+	//取array最后一个
+	$lastName = array_pop($queueArray);
+	$firstSplit = stripos($lastName,"<");
+	$lastName = substr($lastName,0,length);
+				
 	$target = trim($_POST["target"]);
+	
+	$last = strrpos($response,"、");
+	
 	?>
 	<div>
 		<form action="index.php" method="post">
@@ -27,7 +50,7 @@
 		<?php 
 		if(0 != strcasecmp("",$queue) && 0 != strcasecmp("",$target))
 		{
-			$queueArray = explode("、", $queue);
+			
 			echo "没有去重的总人数是".count($queueArray)."人<br/>";
 
 			$queueArrayNoDup = array_unique($queueArray);
--- shouyi.php
+++ shouyi.php
@@ -1,37 +1,38 @@
 <html>
-<head>
-</head>
-<body>
-<?php 
-//年化收益率=一年收益/本金×100%
-//一年天数=365
-//收益=(年华收益率×本金×天数)/一年天数
-//年华收益率=(收益×一年天数)/(本金×天数)
-
-$shouyi = $_POST["shouyi"];
-$benjin = $_POST["benjin"];
-$day = $_POST["day"];
-
-$year = 365;
-$nianhua = 0;
-if(0 != strcasecmp("",$shouyi) && 0 != strcasecmp("",$benjin)&& 0 != strcasecmp("",$day))
-{
-	$nianhua = ($shouyi * $year)/($benjin * $day);
-}
-?>
-
-<div>
-	<form action="shouyi.php" method="post">
-		本金：<input type="text" name="benjin" value="<?php echo $benjin?>"/><br/>
-		天数：<input type="text" name="day" value="<?php echo $day?>"/><br/>
-		收益：<input type="text" name="shouyi" value="<?php echo $shouyi?>"/><br/>
+	<head>
+	</head>
+	<body>
+		ver:1.0
+		<?php 
+		//年化收益率=一年收益/本金×100%
+		//一年天数=365
+		//收益=(年华收益率×本金×天数)/一年天数
+		//年华收益率=(收益×一年天数)/(本金×天数)
 		
-		<input type="submit" value="计算年化收益率"/>		
-	</form>
-</div>
-
-<div>
-		年华收益率：<?php echo $nianhua?>
-</div>
-</body>
+		$shouyi = $_POST["shouyi"];
+		$benjin = $_POST["benjin"];
+		$day = $_POST["day"];
+		
+		$year = 365;
+		$nianhua = 0;
+		if(0 != strcasecmp("",$shouyi) && 0 != strcasecmp("",$benjin)&& 0 != strcasecmp("",$day))
+		{
+			$nianhua = ($shouyi * $year)/($benjin * $day);
+		}
+		?>
+		
+		<div>
+			<form action="shouyi.php" method="post">
+				本金：<input type="text" name="benjin" value="<?php echo $benjin?>"/><br/>
+				天数：<input type="text" name="day" value="<?php echo $day?>"/><br/>
+				收益：<input type="text" name="shouyi" value="<?php echo $shouyi?>"/><br/>
+				
+				<input type="submit" value="计算年化收益率"/>		
+			</form>
+		</div>
+		
+		<div>
+				年华收益率：<?php echo $nianhua?>
+		</div>
+	</body>
 </html>
